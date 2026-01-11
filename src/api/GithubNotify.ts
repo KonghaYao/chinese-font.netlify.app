032a82d7d1e8d2203e275dfb8364a1179fa80fb9
江夏尧 <3446798488@qq.com>
2026-01-11 16:55:56 +0800

    fix: 修复 github 密钥泄露代码

--- src/api/GithubNotify.ts
+++ src/api/GithubNotify.ts
@@ -1,6 +1,7 @@
-
 export class GithubNotify {
-    constructor(public owner: string, public repo: string, public token: string) { }
+    constructor(
+        public owner: string,
+        public repo: string,
+        public token: string
+    ) {}
 }
-export const github = new GithubNotify('KonghaYao', 'chinese-free-web-font-storage',
-    import.meta.env.PUBLIC_GITHUB_TOKEN);
